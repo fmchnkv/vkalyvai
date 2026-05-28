c97f1ce4800f74cb6ee728dac261f35eb5ecea65
assykot48 <assykot48@gmail.com>
2026-05-28 20:02:02 +0500

    deals start

--- template/lk/lk_sidebar.php
+++ template/lk/lk_sidebar.php
@@ -1,6 +1,6 @@
 <nav class="lk__sidebar">
     <ul>
-        <li><a href="javascript:void(0)">Профиль</a></li>
+        <li class="active"><a href="javascript:void(0)">Профиль</a></li>
         <li><a href="javascript:void(0)">Мои резюме</a></li>
         <li><a href="javascript:void(0)">Уведомления</a></li>
         <li><a href="javascript:void(0)">Мои отклики</a></li>
