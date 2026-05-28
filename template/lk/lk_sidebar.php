d43f6a85ae6b5812f991604f69cf976c6706538b
assykot48 <assykot48@gmail.com>
2026-05-28 20:02:02 +0500

    deals start fix

--- template/lk/lk_sidebar.php
+++ template/lk/lk_sidebar.php
@@ -1,10 +1,6 @@
 <nav class="lk__sidebar">
     <ul>
-<<<<<<< HEAD
         <li class="active"><a href="javascript:void(0)">Профиль</a></li>
-=======
-        <li><a href="javascript:void(0)">Профиль</a></li>
->>>>>>> 8c02044 (lk index)
         <li><a href="javascript:void(0)">Мои резюме</a></li>
         <li><a href="javascript:void(0)">Уведомления</a></li>
         <li><a href="javascript:void(0)">Мои отклики</a></li>
