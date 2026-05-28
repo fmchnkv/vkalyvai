8c02044e5be3f4096e60e240ff42e87717c3b079
assykot48 <assykot48@gmail.com>
2026-05-28 19:12:08 +0500

    lk index

---
+++ template/lk/lk_sidebar.php
@@ -0,0 +1,9 @@
+<nav class="lk__sidebar">
+    <ul>
+        <li><a href="javascript:void(0)">Профиль</a></li>
+        <li><a href="javascript:void(0)">Мои резюме</a></li>
+        <li><a href="javascript:void(0)">Уведомления</a></li>
+        <li><a href="javascript:void(0)">Мои отклики</a></li>
+        <li><a href="javascript:void(0)">Избранное</a></li>
+    </ul>
+</nav>
