83058946ed434a4a6521bde5e1d7ee465fc7769e
assykot48 <assykot48@gmail.com>
2026-06-03 18:57:23 +0500

    modal

--- template/lk/responces/employer_responces_list.php
+++ template/lk/responces/employer_responces_list.php
@@ -1,9 +1,9 @@
-<div class="employ-responces-list full-width-list">
-    <div class="employ-responces-item lk-card">
-        <span class="employ-responces-item__status state publish">Опубликовано</span>
-        <div class="employ-responces-item__vacancy">Название вакансии</div>
-        <div class="employ-responces-item__body">
-            <div class="employ-responces-item__title">
+<div class="employer__responces-list full-width-list">
+    <div class="employer__responces-item lk-card">
+        <span class="employer__responces-item__status state publish">Опубликовано</span>
+        <div class="employer__responces-item__vacancy">Название вакансии</div>
+        <div class="employer__responces-item__body">
+            <div class="employer__responces-item__title">
                 <h3 class="offer-header__title">
                     <a href="/pages/rezume.php" class="link">Иванов Иван Иванович</a>
                 </h3>
@@ -40,7 +40,7 @@
                 <li class="offer-info__item has-dot">Не готов к командировкам</li>
             </ul>
         </div>
-        <div class="employ-responces-item__footer">
+        <div class="employer__responces-item__footer">
             <div class="buttons-wrapper">
                 <button class="chat__btn btn btn_primary btn_size-m">Чат</button>
                 <button class="accept__btn btn btn_light btn_size-m btn_has-icon">
@@ -100,11 +100,11 @@
     </div>
 
 
-    <div class="employ-responces-item lk-card">
-        <span class="employ-responces-item__status state blocked">Отказ</span>
-        <div class="employ-responces-item__vacancy">Название вакансии</div>
-        <div class="employ-responces-item__body">
-            <div class="employ-responces-item__title">
+    <div class="employer__responces-item lk-card">
+        <span class="employer__responces-item__status state blocked">Отказ</span>
+        <div class="employer__responces-item__vacancy">Название вакансии</div>
+        <div class="employer__responces-item__body">
+            <div class="employer__responces-item__title">
                 <h3 class="offer-header__title">
                     <a href="/pages/rezume.php" class="link">Иванов Иван Иванович</a>
                 </h3>
@@ -141,7 +141,7 @@
                 <li class="offer-info__item has-dot">Не готов к командировкам</li>
             </ul>
         </div>
-        <div class="employ-responces-item__footer">
+        <div class="employer__responces-item__footer">
             <div class="buttons-wrapper">
                 <button class="chat__btn btn btn_primary btn_size-m">Чат</button>
                 <button class="accept__btn btn btn_light btn_size-m btn_has-icon">
@@ -201,11 +201,11 @@
     </div>
 
 
-    <div class="employ-responces-item lk-card">
-        <span class="employ-responces-item__status state neutral_see">Просмотрен</span>
-        <div class="employ-responces-item__vacancy">Название вакансии</div>
-        <div class="employ-responces-item__body">
-            <div class="employ-responces-item__title">
+    <div class="employer__responces-item lk-card">
+        <span class="employer__responces-item__status state neutral_see">Просмотрен</span>
+        <div class="employer__responces-item__vacancy">Название вакансии</div>
+        <div class="employer__responces-item__body">
+            <div class="employer__responces-item__title">
                 <h3 class="offer-header__title">
                     <a href="/pages/rezume.php" class="link">Иванов Иван Иванович</a>
                 </h3>
@@ -242,7 +242,7 @@
                 <li class="offer-info__item has-dot">Не готов к командировкам</li>
             </ul>
         </div>
-        <div class="employ-responces-item__footer">
+        <div class="employer__responces-item__footer">
             <div class="buttons-wrapper">
                 <button class="chat__btn btn btn_primary btn_size-m">Чат</button>
                 <button class="accept__btn btn btn_light btn_size-m btn_has-icon">
@@ -302,11 +302,11 @@
     </div>
 
 
-    <div class="employ-responces-item lk-card">
-        <span class="employ-responces-item__status state neutral">Просмотрен</span>
-        <div class="employ-responces-item__vacancy">Название вакансии</div>
-        <div class="employ-responces-item__body">
-            <div class="employ-responces-item__title">
+    <div class="employer__responces-item lk-card">
+        <span class="employer__responces-item__status state neutral">Просмотрен</span>
+        <div class="employer__responces-item__vacancy">Название вакансии</div>
+        <div class="employer__responces-item__body">
+            <div class="employer__responces-item__title">
                 <h3 class="offer-header__title">
                     <a href="/pages/rezume.php" class="link">Иванов Иван Иванович</a>
                 </h3>
@@ -343,7 +343,7 @@
                 <li class="offer-info__item has-dot">Не готов к командировкам</li>
             </ul>
         </div>
-        <div class="employ-responces-item__footer">
+        <div class="employer__responces-item__footer">
             <div class="buttons-wrapper">
                 <button class="chat__btn btn btn_primary btn_size-m">Чат</button>
                 <button class="accept__btn btn btn_light btn_size-m btn_has-icon">
@@ -402,4 +402,65 @@
         </div>
     </div>
 
+</div>
+<div class="responces__pagination pagination">
+    <a href="#" class="pagination__arrow pagination__arrow_prev" style="display: none;">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_4580_14223)">
+                <path d="M10.3057 3.63764C10.5986 3.34475 11.0733 3.34475 11.3662 3.63764L17.1992 9.47065C17.4921 9.76354 17.4921 10.2383 17.1992 10.5312L11.3662 16.3652C11.0733 16.6575 10.5984 16.6577 10.3057 16.3652C10.0129 16.0724 10.0133 15.5976 10.3057 15.3046L14.8584 10.7509H3.33594C2.92198 10.7507 2.58611 10.4149 2.58594 10.0009C2.58611 9.58696 2.92198 9.2511 3.33594 9.25092H14.8584L10.3057 4.69819C10.0129 4.4054 10.0131 3.93056 10.3057 3.63764Z" fill="#999999"></path>
+            </g>
+            <defs>
+                <clipPath id="clip0_4580_14223">
+                    <rect width="20" height="20" fill="white"></rect>
+                </clipPath>
+            </defs>
+        </svg>
+    </a>
+    <ul class="pagination__list">
+        <li class="pagination__item">
+            <a href="#" class="pagination__link active">1</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">2</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">3</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">4</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">5</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">6</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">7</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">8</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">9</a>
+        </li>
+        <li class="pagination__item pagination__item_dots">
+            <a href="#" class="pagination__link pagination__link_dots">...</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">23</a>
+        </li>
+    </ul>
+    <a href="#" class="pagination__arrow pagination__arrow_next">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_4580_14223)">
+                <path d="M10.3057 3.63764C10.5986 3.34475 11.0733 3.34475 11.3662 3.63764L17.1992 9.47065C17.4921 9.76354 17.4921 10.2383 17.1992 10.5312L11.3662 16.3652C11.0733 16.6575 10.5984 16.6577 10.3057 16.3652C10.0129 16.0724 10.0133 15.5976 10.3057 15.3046L14.8584 10.7509H3.33594C2.92198 10.7507 2.58611 10.4149 2.58594 10.0009C2.58611 9.58696 2.92198 9.2511 3.33594 9.25092H14.8584L10.3057 4.69819C10.0129 4.4054 10.0131 3.93056 10.3057 3.63764Z" fill="#999999"></path>
+            </g>
+            <defs>
+                <clipPath id="clip0_4580_14223">
+                    <rect width="20" height="20" fill="white"></rect>
+                </clipPath>
+            </defs>
+        </svg>
+    </a>
 </div>
