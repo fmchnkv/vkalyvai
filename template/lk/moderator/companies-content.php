48586c2f08ce8e5f3a524b20a37ca9ec34bee8f2
assykot48 <assykot48@gmail.com>
2026-06-08 20:04:59 +0500

    лк модератора (без последней страницы)

---
+++ template/lk/moderator/companies-content.php
@@ -0,0 +1,270 @@
+<h1 class="moderator__vacancies-title page-title lk__page-title">Компании</h1>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/template/lk/responces/responces-search.php"); ?>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/template/lk/moderator/sections-list.php"); ?>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/template/lk/responces/responces_sort.php"); ?>
+
+<div class="moderator__list grid-list">
+    <div class="offers-grid__item moderator__item lk-card">
+        <span class="moderator__item__status state publish">Новый пользователь</span>
+        <div class="moderator__item__date-wrapper">
+            <div class="date">01.06.2397</div>
+            <div class="offer-info__item">
+                <img src="/images/geo-icon.svg" alt="geo icon">
+                <span>Город, район, метро</span>
+            </div>
+        </div>
+        <div class="moderator__item__title-wrapper">
+            <div class="moderator__img">
+                <img src="/images/company-avatar.png" alt="">
+            </div>
+            <a href="/pages/vacancy.php" class="offers-grid__title">Название компании</a>
+            <ul class="offers-grid__tags tags">
+                <li class="offer-header__main-item has-dot">64 активных вакансии</li>
+                <li class="offer-header__main-item has-dot">Был сегодня</li>
+            </ul>
+        </div>
+        <ul class="offers-list__info offer-info">
+            <li class="offer-info__item">
+                <img src="/images/company-icon.svg" alt="company icon">
+                <span>Медицинская клиника</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/settings-icon.svg" alt="company icon">
+                <span>Организация</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/network-icon.svg" alt="company icon">
+                <span>https://blueberry-digital.ru/</span>
+            </li>
+        </ul>
+        <div class="offers-grid__bottom">
+            <div class="offers-grid__actions offer-actions">
+                <a class="lk__btn_has-icon transp-btn btn--show">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path
+                            d="M16.7539 17.501H3.25391V16.001H16.7539V17.501ZM12.3604 2.52246C12.5591 2.52246 12.75 2.60168 12.8906 2.74219L15.0127 4.86426C15.1532 5.00489 15.2324 5.19573 15.2324 5.39453C15.2324 5.59334 15.1532 5.78418 15.0127 5.9248L6.43652 14.501H3.25391V11.3184L11.8301 2.74219C11.9707 2.60166 12.1615 2.52248 12.3604 2.52246ZM4.75391 11.9404V13.001H5.81445L13.4209 5.39453L12.3604 4.33398L4.75391 11.9404Z"
+                            fill="#FC7827" />
+                    </svg>
+                    <span>Смотреть</span>
+                </a>
+            </div>
+            <button class="lk__btn_has-icon transp-btn btn--block">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <g clip-path="url(#clip0_8333_34494)">
+                        <path
+                            d="M13.7539 4.00098H17.5039V5.50098H16.0039V16.751C16.0039 16.9499 15.9248 17.1406 15.7842 17.2812C15.6435 17.4219 15.4528 17.501 15.2539 17.501H4.75391C4.55499 17.501 4.36429 17.4219 4.22363 17.2812C4.08298 17.1406 4.00391 16.9499 4.00391 16.751V5.50098H2.50391V4.00098H6.25391V2.50098H13.7539V4.00098ZM5.50391 16.001H14.5039V5.50098H5.50391V16.001ZM9.25391 13.751H7.75391V7.75098H9.25391V13.751ZM12.2539 13.751H10.7539V7.75098H12.2539V13.751Z"
+                            fill="#F14C4C" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_8333_34494">
+                            <rect width="20" height="20" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+                <span>Заблокировать</span>
+            </button>
+        </div>
+    </div>
+    <div class="offers-grid__item moderator__item lk-card">
+        <span class="moderator__item__status state neutral">Не просмотрено</span>
+        <div class="moderator__item__date-wrapper">
+            <div class="date">01.06.2397</div>
+            <div class="offer-info__item">
+                <img src="/images/geo-icon.svg" alt="geo icon">
+                <span>Город, район, метро</span>
+            </div>
+        </div>
+        <div class="moderator__item__title-wrapper">
+            <div class="moderator__img">
+                <img src="/images/company-avatar.png" alt="">
+            </div>
+            <a href="/pages/vacancy.php" class="offers-grid__title">Название компании</a>
+            <ul class="offers-grid__tags tags">
+                <li class="offer-header__main-item has-dot">64 активных вакансии</li>
+                <li class="offer-header__main-item has-dot">Был сегодня</li>
+            </ul>
+        </div>
+        <ul class="offers-list__info offer-info">
+            <li class="offer-info__item">
+                <img src="/images/company-icon.svg" alt="company icon">
+                <span>Медицинская клиника</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/settings-icon.svg" alt="company icon">
+                <span>Организация</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/network-icon.svg" alt="company icon">
+                <span>https://blueberry-digital.ru/</span>
+            </li>
+        </ul>
+        <div class="offers-grid__bottom">
+            <div class="offers-grid__actions offer-actions">
+                <button class="lk__btn_has-icon transp-btn btn--publish">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <g clip-path="url(#clip0_8333_33775)">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M14.9024 5.75787C15.2929 5.36746 15.926 5.36753 16.3165 5.75787C16.7069 6.14824 16.7076 6.78137 16.3175 7.17193L9.24716 14.2432C9.05974 14.4307 8.8052 14.536 8.54013 14.5362C8.27494 14.5362 8.02064 14.4307 7.8331 14.2432L4.29697 10.7081C3.90649 10.3176 3.90659 9.68454 4.29697 9.294C4.6875 8.9036 5.32055 8.90352 5.71103 9.294L8.53916 12.1221L14.9024 5.75787Z"
+                                fill="#5DB634" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_8333_33775">
+                                <rect width="20" height="20" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                    <span>Опубликовать</span>
+                </button>
+                <a class="lk__btn_has-icon transp-btn btn--show">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path
+                            d="M16.7539 17.501H3.25391V16.001H16.7539V17.501ZM12.3604 2.52246C12.5591 2.52246 12.75 2.60168 12.8906 2.74219L15.0127 4.86426C15.1532 5.00489 15.2324 5.19573 15.2324 5.39453C15.2324 5.59334 15.1532 5.78418 15.0127 5.9248L6.43652 14.501H3.25391V11.3184L11.8301 2.74219C11.9707 2.60166 12.1615 2.52248 12.3604 2.52246ZM4.75391 11.9404V13.001H5.81445L13.4209 5.39453L12.3604 4.33398L4.75391 11.9404Z"
+                            fill="#FC7827" />
+                    </svg>
+                    <span>Смотреть</span>
+                </a>
+            </div>
+            <button class="lk__btn_has-icon transp-btn btn--block">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <g clip-path="url(#clip0_8333_34494)">
+                        <path
+                            d="M13.7539 4.00098H17.5039V5.50098H16.0039V16.751C16.0039 16.9499 15.9248 17.1406 15.7842 17.2812C15.6435 17.4219 15.4528 17.501 15.2539 17.501H4.75391C4.55499 17.501 4.36429 17.4219 4.22363 17.2812C4.08298 17.1406 4.00391 16.9499 4.00391 16.751V5.50098H2.50391V4.00098H6.25391V2.50098H13.7539V4.00098ZM5.50391 16.001H14.5039V5.50098H5.50391V16.001ZM9.25391 13.751H7.75391V7.75098H9.25391V13.751ZM12.2539 13.751H10.7539V7.75098H12.2539V13.751Z"
+                            fill="#F14C4C" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_8333_34494">
+                            <rect width="20" height="20" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+                <span>Заблокировать</span>
+            </button>
+        </div>
+    </div>
+    <div class="offers-grid__item moderator__item lk-card">
+        <span class="moderator__item__status state blocked">Заблокировано</span>
+        <div class="moderator__item__date-wrapper">
+            <div class="date">01.06.2397</div>
+            <div class="offer-info__item">
+                <img src="/images/geo-icon.svg" alt="geo icon">
+                <span>Город, район, метро</span>
+            </div>
+        </div>
+        <div class="moderator__item__title-wrapper">
+            <div class="moderator__img">
+                <img src="/images/company-avatar.png" alt="">
+            </div>
+            <a href="/pages/vacancy.php" class="offers-grid__title">Название компании</a>
+            <ul class="offers-grid__tags tags">
+                <li class="offer-header__main-item has-dot">64 активных вакансии</li>
+                <li class="offer-header__main-item has-dot">Был сегодня</li>
+            </ul>
+        </div>
+        <ul class="offers-list__info offer-info">
+            <li class="offer-info__item">
+                <img src="/images/company-icon.svg" alt="company icon">
+                <span>Медицинская клиника</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/settings-icon.svg" alt="company icon">
+                <span>Организация</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/network-icon.svg" alt="company icon">
+                <span>https://blueberry-digital.ru/</span>
+            </li>
+        </ul>
+        <div class="offers-grid__bottom">
+            <div class="offers-grid__actions offer-actions">
+                <a class="lk__btn_has-icon transp-btn btn--show">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path
+                            d="M16.7539 17.501H3.25391V16.001H16.7539V17.501ZM12.3604 2.52246C12.5591 2.52246 12.75 2.60168 12.8906 2.74219L15.0127 4.86426C15.1532 5.00489 15.2324 5.19573 15.2324 5.39453C15.2324 5.59334 15.1532 5.78418 15.0127 5.9248L6.43652 14.501H3.25391V11.3184L11.8301 2.74219C11.9707 2.60166 12.1615 2.52248 12.3604 2.52246ZM4.75391 11.9404V13.001H5.81445L13.4209 5.39453L12.3604 4.33398L4.75391 11.9404Z"
+                            fill="#FC7827" />
+                    </svg>
+                    <span>Смотреть</span>
+                </a>
+            </div>
+            <button class="lk__btn_has-icon transp-btn btn--delete">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <g clip-path="url(#clip0_8333_34494)">
+                        <path
+                            d="M13.7539 4.00098H17.5039V5.50098H16.0039V16.751C16.0039 16.9499 15.9248 17.1406 15.7842 17.2812C15.6435 17.4219 15.4528 17.501 15.2539 17.501H4.75391C4.55499 17.501 4.36429 17.4219 4.22363 17.2812C4.08298 17.1406 4.00391 16.9499 4.00391 16.751V5.50098H2.50391V4.00098H6.25391V2.50098H13.7539V4.00098ZM5.50391 16.001H14.5039V5.50098H5.50391V16.001ZM9.25391 13.751H7.75391V7.75098H9.25391V13.751ZM12.2539 13.751H10.7539V7.75098H12.2539V13.751Z"
+                            fill="#F14C4C" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_8333_34494">
+                            <rect width="20" height="20" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+                <span>Скрыть из списка</span>
+            </button>
+        </div>
+    </div>
+    <div class="offers-grid__item moderator__item lk-card">
+        <span class="moderator__item__status state working">На доработке</span>
+        <div class="moderator__item__date-wrapper">
+            <div class="date">01.06.2397</div>
+            <div class="offer-info__item">
+                <img src="/images/geo-icon.svg" alt="geo icon">
+                <span>Город, район, метро</span>
+            </div>
+        </div>
+        <div class="moderator__item__title-wrapper">
+            <div class="moderator__img">
+                <img src="/images/company-avatar.png" alt="">
+            </div>
+            <a href="/pages/vacancy.php" class="offers-grid__title">Название компании</a>
+            <ul class="offers-grid__tags tags">
+                <li class="offer-header__main-item has-dot">64 активных вакансии</li>
+                <li class="offer-header__main-item has-dot">Был сегодня</li>
+            </ul>
+        </div>
+        <ul class="offers-list__info offer-info">
+            <li class="offer-info__item">
+                <img src="/images/company-icon.svg" alt="company icon">
+                <span>Медицинская клиника</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/settings-icon.svg" alt="company icon">
+                <span>Организация</span>
+            </li>
+            <li class="offer-info__item">
+                <img src="/images/network-icon.svg" alt="company icon">
+                <span>https://blueberry-digital.ru/</span>
+            </li>
+        </ul>
+        <div class="offers-grid__bottom">
+            <div class="offers-grid__actions offer-actions">
+                <a class="lk__btn_has-icon transp-btn btn--show">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path
+                            d="M16.7539 17.501H3.25391V16.001H16.7539V17.501ZM12.3604 2.52246C12.5591 2.52246 12.75 2.60168 12.8906 2.74219L15.0127 4.86426C15.1532 5.00489 15.2324 5.19573 15.2324 5.39453C15.2324 5.59334 15.1532 5.78418 15.0127 5.9248L6.43652 14.501H3.25391V11.3184L11.8301 2.74219C11.9707 2.60166 12.1615 2.52248 12.3604 2.52246ZM4.75391 11.9404V13.001H5.81445L13.4209 5.39453L12.3604 4.33398L4.75391 11.9404Z"
+                            fill="#FC7827" />
+                    </svg>
+                    <span>Смотреть</span>
+                </a>
+            </div>
+            <button class="lk__btn_has-icon transp-btn btn--block">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <g clip-path="url(#clip0_8333_34494)">
+                        <path
+                            d="M13.7539 4.00098H17.5039V5.50098H16.0039V16.751C16.0039 16.9499 15.9248 17.1406 15.7842 17.2812C15.6435 17.4219 15.4528 17.501 15.2539 17.501H4.75391C4.55499 17.501 4.36429 17.4219 4.22363 17.2812C4.08298 17.1406 4.00391 16.9499 4.00391 16.751V5.50098H2.50391V4.00098H6.25391V2.50098H13.7539V4.00098ZM5.50391 16.001H14.5039V5.50098H5.50391V16.001ZM9.25391 13.751H7.75391V7.75098H9.25391V13.751ZM12.2539 13.751H10.7539V7.75098H12.2539V13.751Z"
+                            fill="#F14C4C" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_8333_34494">
+                            <rect width="20" height="20" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+                <span>Заблокировать</span>
+            </button>
+        </div>
+    </div>
+</div>
+
+<? include $_SERVER["DOCUMENT_ROOT"] . '/include/pagination.php'; ?>
---
+++ template/lk/moderator/sections-list.php
@@ -0,0 +1,20 @@
+<ul class="moderator__vacancies-tabs lk__tabs tabs">
+    <li class="tabs__item">
+        <a href="javascript:void(0);" class="tabs__link tab active">Все</a>
+    </li>
+    <li class="tabs__item">
+        <a href="javascript:void(0);" class="tabs__link tab">Не просмотрены</a>
+    </li>
+    <li class="tabs__item">
+        <a href="javascript:void(0);" class="tabs__link tab">Просмотрены</a>
+    </li>
+    <li class="tabs__item">
+        <a href="javascript:void(0);" class="tabs__link tab">Опубликованы</a>
+    </li>
+    <li class="tabs__item">
+        <a href="javascript:void(0);" class="tabs__link tab">На доработке</a>
+    </li>
+    <li class="tabs__item">
+        <a href="javascript:void(0);" class="tabs__link tab">Заблокированы</a>
+    </li>
+</ul>
