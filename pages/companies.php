465b8ca685e735cfaf0252d47cc65cddf78b74d6
fmchnkv <1vasilisk1@gmail.com>
2026-06-22 23:22:37 +0800

    Исправление страниц каталогов вакансии резюме компании

--- pages/companies.php
+++ pages/companies.php
@@ -18,17 +18,12 @@
     <div class="container">
         <div class="catalog-nav__inner">
             <div class="catalog-nav__actions">
-                <button class="catalog-nav__nav-btn btn btn_outline btn_size-s btn_has-icon">
+                <button class="catalog-nav__nav-btn btn btn_outline btn_size-m btn_has-icon">
                     <span>Компании</span>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g >
-                            <path d="M10.0048 2.58398C10.4188 2.58416 10.7547 2.92003 10.7548 3.33398V14.8564L15.3085 10.3037C15.6015 10.0113 16.0764 10.011 16.3691 10.3037C16.6617 10.5965 16.6614 11.0714 16.3691 11.3643L10.5351 17.1973C10.2422 17.4902 9.76745 17.4902 9.47455 17.1973L3.64154 11.3643C3.34865 11.0714 3.34865 10.5966 3.64154 10.3037C3.93446 10.0111 4.40931 10.0109 4.70209 10.3037L9.25483 14.8564V3.33398C9.255 2.92003 9.59087 2.58416 10.0048 2.58398Z" fill="#FC7827" />
+                        <g>
+                            <path d="M14.4735 7.38764C14.7664 7.09475 15.2411 7.09475 15.534 7.38764C15.8268 7.68055 15.8269 8.15535 15.534 8.44819L10.534 13.4482C10.2595 13.7226 9.82466 13.7401 9.53014 13.4999L9.4735 13.4482L4.4735 8.44819C4.18066 8.15535 4.18076 7.68055 4.4735 7.38764C4.76639 7.09475 5.24115 7.09475 5.53404 7.38764L10.0038 11.8574L14.4735 7.38764Z" fill="#FC7827" />
                         </g>
-                        <defs>
-                            <clipPath id="clip0_4580_8105">
-                                <rect width="20" height="20" fill="white" />
-                            </clipPath>
-                        </defs>
                     </svg>
                 </button>
 
@@ -49,8 +44,8 @@
                 <div class="inline-form__row">
                     <div class="inline-form__input-wrapper">
                         <label class="inline-form__label field">
-                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g >
+                            <svg class="field__icon" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g>
                                     <path d="M11.1035 3.00098C15.5746 3.00098 19.2039 6.62957 19.2041 11.1006C19.2067 12.9384 18.5811 14.7223 17.4316 16.1562L21.2861 20.0098L20.0127 21.2832L16.1592 17.4287C14.7252 18.5782 12.9413 19.2038 11.1035 19.2012C6.6325 19.201 3.00391 15.5717 3.00391 11.1006C3.00412 6.6297 6.63263 3.00119 11.1035 3.00098ZM11.1035 4.80078C7.62263 4.80099 4.80392 7.6197 4.80371 11.1006C4.80371 14.5808 7.6225 17.4012 11.1035 17.4014C12.7419 17.4039 14.3166 16.7652 15.4912 15.623L15.626 15.4883C16.7682 14.3137 17.4069 12.739 17.4043 11.1006C17.4041 7.61957 14.5837 4.80078 11.1035 4.80078Z" fill="#FC7827" />
                                 </g>
                                 <defs>
@@ -66,7 +61,7 @@
                 <button class="inline-form__btn btn btn_primary btn_size-s">
                     <span>Найти</span>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g >
+                        <g>
                             <path d="M9.25391 2.50098C12.9799 2.50098 16.0039 5.52498 16.0039 9.25098C16.0061 10.7825 15.4853 12.2689 14.5273 13.4639L17.7393 16.6748L16.6777 17.7363L13.4668 14.5244C12.2719 15.4823 10.7854 16.0032 9.25391 16.001C5.52791 16.001 2.50391 12.977 2.50391 9.25098C2.50391 5.52498 5.52791 2.50098 9.25391 2.50098ZM9.25391 4.00098C6.35291 4.00098 4.00391 6.34998 4.00391 9.25098C4.00391 12.1512 6.35291 14.501 9.25391 14.501C10.6192 14.5031 11.9313 13.9714 12.9102 13.0195L13.0225 12.9072C13.9743 11.9284 14.5061 10.6163 14.5039 9.25098C14.5039 6.34998 12.1542 4.00098 9.25391 4.00098Z" fill="white" />
                         </g>
                         <defs>
@@ -89,7 +84,7 @@
                 <span class="catalog-header__counter">Найдено 16 177 компаний</span>
 
                 <div class="catalog-header__sorting sorting">
-                    <? 
+                    <?
                     /* 
                     
                     <div class="sorting__item">
@@ -172,13 +167,13 @@
                     <div class="sorting__item">
                         <button class="sorting__button accordion-button btn-link js-sort-btn">
                             <span>По количеству вакансий</span>
-                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g >
+                            <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g>
                                     <path d="M10.0009 2.58301C10.4149 2.58318 10.7507 2.91905 10.7509 3.33301V14.8555L15.3046 10.3027C15.5976 10.0103 16.0724 10.01 16.3652 10.3027C16.6577 10.5955 16.6575 11.0704 16.3652 11.3633L10.5312 17.1963C10.2383 17.4892 9.76354 17.4892 9.47065 17.1963L3.63764 11.3633C3.34475 11.0704 3.34475 10.5956 3.63764 10.3027C3.93056 10.0102 4.4054 10.0099 4.69819 10.3027L9.25092 14.8555V3.33301C9.2511 2.91905 9.58696 2.58318 10.0009 2.58301Z" fill="#FC7827" />
                                 </g>
                                 <defs>
                                     <clipPath id="clip0_4580_8402">
-                                        <rect width="20" height="20" fill="white" />
+                                        <rect width="16" height="16" fill="white" />
                                     </clipPath>
                                 </defs>
                             </svg>
@@ -195,13 +190,13 @@
                     <div class="sorting__item">
                         <button class="sorting__button accordion-button btn-link js-sort-btn">
                             <span>Все отрасли</span>
-                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g >
+                            <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g>
                                     <path d="M10.0009 2.58301C10.4149 2.58318 10.7507 2.91905 10.7509 3.33301V14.8555L15.3046 10.3027C15.5976 10.0103 16.0724 10.01 16.3652 10.3027C16.6577 10.5955 16.6575 11.0704 16.3652 11.3633L10.5312 17.1963C10.2383 17.4892 9.76354 17.4892 9.47065 17.1963L3.63764 11.3633C3.34475 11.0704 3.34475 10.5956 3.63764 10.3027C3.93056 10.0102 4.4054 10.0099 4.69819 10.3027L9.25092 14.8555V3.33301C9.2511 2.91905 9.58696 2.58318 10.0009 2.58301Z" fill="#FC7827" />
                                 </g>
                                 <defs>
                                     <clipPath id="clip0_4580_8402">
-                                        <rect width="20" height="20" fill="white" />
+                                        <rect width="16" height="16" fill="white" />
                                     </clipPath>
                                 </defs>
                             </svg>
@@ -227,10 +222,13 @@
         </div>
 
         <ul class="catalog__list companies-grid js-list" data-id="companies">
-            <? for ($i = 0; $i < 12; $i++) { include $_SERVER["DOCUMENT_ROOT"] . '/include/company-item.php'; } ?>
+            <? for ($i = 0; $i < 12; $i++) {
+                include $_SERVER["DOCUMENT_ROOT"] . '/include/company-item.php';
+            } ?>
         </ul>
         <div class="scroll-trigger"></div>
-        <? // include $_SERVER["DOCUMENT_ROOT"] . '/include/pagination.php'; ?>
+        <? // include $_SERVER["DOCUMENT_ROOT"] . '/include/pagination.php'; 
+        ?>
     </div>
 </section>
 
